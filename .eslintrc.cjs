f12fab3028dab1889cb155e93b03a02bfbb4fc46
Riceball <42161586+Anderice@users.noreply.github.com>
2022-11-30 06:53:47 +0800

    feat: add @ alias path

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,14 @@ module.exports = {
     node: true,
   },
   extends: ['airbnb-base', 'plugin:vue/vue3-recommended', 'prettier'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.vue'],
+      },
+    },
+  },
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
