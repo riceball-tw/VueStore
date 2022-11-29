20f8046d0f1c32e5c57036fa02a54c09132d9f79
Riceball <42161586+Anderice@users.noreply.github.com>
2022-11-30 06:55:25 +0800

    feat: seperate navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <ul class="bg-red-300">
+    <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+    <li><router-link :to="{ name: 'about' }">About</router-link></li>
+    <li><router-link :to="{ name: 'dashboard' }">Dashboard</router-link></li>
+    <li><router-link :to="{ name: 'login' }">Login</router-link></li>
+    <li><router-link :to="{ name: 'login' }">Logout</router-link></li>
+  </ul>
+</template>
