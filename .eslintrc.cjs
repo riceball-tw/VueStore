e149e33420dd3442336ba9a9d22e3c161b6c4907
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-04 07:07:45 +0800

    config: linter fileName rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
     },
   },
   rules: {
+    'vue/multi-word-component-names': 'off',
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
   },
