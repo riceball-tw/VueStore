603576f05e7569d2d4ed5876b86d3f7a108dc1e1
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-09 07:16:18 +0800

    refactor: rewrite to axios

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -11,22 +11,19 @@
 <script setup>
 import getAuthToken from '@/helper/getAuthToken';
 import { useToast } from 'vue-toastification';
-
-async function logout(authToken) {
-  const response = await fetch(`${import.meta.env.VITE_APP_API}/logout`, {
-    method: 'POST',
-    headers: { 'content-type': 'application/json', Authorization: authToken },
-  });
-  if (!response.ok) throw new Error(`發生錯誤，${response.status}`);
-  const responseJSON = await response.json();
-  if (!responseJSON.success) throw new Error(`發生錯誤，${responseJSON.message}`);
-  return responseJSON;
-}
+import axios from 'axios';
 
 function logoutClicked() {
-  logout(getAuthToken)
-    .then((responseJSON) => {
-      useToast().success(`${responseJSON.message}`);
+  axios({
+    method: 'post',
+    url: `${import.meta.env.VITE_APP_API}/logout`,
+    headers: {
+      Authorization: getAuthToken,
+    },
+  })
+    .then((res) => {
+      if (!res.data.success) throw new Error(`${res.data.message}`);
+      useToast().success(`${res.data.message}`);
     })
     .catch((err) => {
       useToast().error(`${err.message}`);
