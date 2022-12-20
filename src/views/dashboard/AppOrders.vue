43c0ea89f63538d4cb99c1be634735922546c003
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-20 23:14:53 +0800

    refactor: move file location

---
+++ src/views/dashboard/AppOrders.vue
@@ -0,0 +1,20 @@
+<template>
+  <Suspense>
+    <template #default>
+      <DashboardOrders />
+    </template>
+    <template #fallback>
+      <h1>產品加載中</h1>
+    </template>
+  </Suspense>
+</template>
+
+<script setup>
+import { onErrorCaptured } from 'vue';
+import { useToast } from 'vue-toastification';
+import DashboardOrders from '@/components/dashboard/AppOrders.vue';
+
+onErrorCaptured((err) => {
+  useToast().error(`${err}`);
+});
+</script>
