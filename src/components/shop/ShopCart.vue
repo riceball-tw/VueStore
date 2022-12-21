ae067f12abf5d093082e8887180ad457474e2b8f
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-21 18:55:36 +0800

    feat: shopCart

--- src/components/shop/ShopCart.vue
+++ src/components/shop/ShopCart.vue
@@ -1,20 +1,49 @@
 <template>
   <h1>商品列表</h1>
-  <table>
+
+  <div v-if="!cartsData.carts.length">目前購物車內無商品</div>
+
+  <div>
+    <button v-if="cartsData.carts.length" @click="deleteAllCart()">刪除全部購物車</button>
+  </div>
+
+  <table v-if="cartsData.carts.length">
     <thead>
       <tr>
-        <th width="120">圖片</th>
-        <th width="120">商品名稱</th>
-        <th width="120">價格</th>
+        <th>品名</th>
+        <th>數量</th>
+        <th>單價</th>
+        <th>編輯</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="cart in carts" :key="cart.id">
-        <td>{{ cart.product.imageUrl }}</td>
-        <td>{{ cart.product.title }}</td>
+      <tr v-for="cart in cartsData.carts" :key="cart.id">
+        <td>{{ cart.product?.title ? cart.product.title : '無紀錄' }}</td>
+        <td class="flex">
+          <input
+            type="number"
+            :value="cart.qty"
+            min="1"
+            :disabled="loadingCart.includes(cart.id)"
+            @change="
+              (e) => {
+                handleEditCart(cart.id, e.target.value);
+              }
+            "
+          />/{{ cart.product?.unit ? cart.product.unit : '個' }}
+        </td>
         <td>{{ cart.total }}</td>
+        <td>
+          <button @click="deleteCart(cart.id)">刪除</button>
+        </td>
       </tr>
     </tbody>
+    <tfoot>
+      <tr>
+        <td>Total: {{ cartsData.total }}</td>
+        <td>FinalTotal: {{ cartsData.final_total }}</td>
+      </tr>
+    </tfoot>
   </table>
 </template>
 
@@ -26,7 +55,10 @@ import getAuthToken from '@/helper/getAuthToken';
 import axios from 'axios';
 
 // UI Data
-const carts = ref([]);
+const cartsData = ref([]);
+
+// Component State
+const loadingCart = ref([]);
 
 // Remotely get carts, then render them
 async function renderCarts() {
@@ -37,7 +69,67 @@ async function renderCarts() {
   })
     .then((res) => {
       if (!res.data.success) throw new Error(`${res.data.message}`);
-      carts.value = res.data.data.carts;
+      cartsData.value = res.data.data;
+    })
+    .catch((err) => {
+      useToast().error(`${err.message}`);
+    });
+}
+
+function editCart(cartId, quantity = 1) {
+  loadingCart.value = [...loadingCart.value, cartId];
+  axios({
+    method: 'put',
+    url: `${import.meta.env.VITE_APP_API}/api/${import.meta.env.VITE_APP_PATH}/cart/${cartId}`,
+    headers: { Authorization: getAuthToken() },
+    data: {
+      data: { product_id: 'cartId', qty: quantity },
+    },
+  })
+    .then((res) => {
+      if (!res.data.success) throw new Error(`${res.data.message}`);
+
+      useToast().success(`${res.data.message}`);
+      renderCarts();
+    })
+    .catch((err) => {
+      useToast().error(`${err.message}`);
+    })
+    .finally(() => {
+      loadingCart.value = loadingCart.value.filter((cart) => cart !== cartId);
+    });
+}
+
+function handleEditCart(cartId, newQuantity = 1) {
+  editCart(cartId, parseInt(newQuantity, 10));
+}
+
+function deleteAllCart() {
+  axios({
+    method: 'delete',
+    url: `${import.meta.env.VITE_APP_API}/api/${import.meta.env.VITE_APP_PATH}/carts`,
+    headers: { Authorization: getAuthToken() },
+  })
+    .then((res) => {
+      if (!res.data.success) throw new Error(`${res.data.message}`);
+      useToast().success(`${res.data.message}`);
+      renderCarts();
+    })
+    .catch((err) => {
+      useToast().error(`${err.message}`);
+    });
+}
+
+function deleteCart(cartId) {
+  axios({
+    method: 'delete',
+    url: `${import.meta.env.VITE_APP_API}/api/${import.meta.env.VITE_APP_PATH}/cart/${cartId}`,
+    headers: { Authorization: getAuthToken() },
+  })
+    .then((res) => {
+      if (!res.data.success) throw new Error(`${res.data.message}`);
+      useToast().success(`${res.data.message}`);
+      renderCarts();
     })
     .catch((err) => {
       useToast().error(`${err.message}`);
