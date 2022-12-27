b1f53910389ca34651d966ef176d47c19d9ab917
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-27 23:00:44 +0800

    feat: Basic Navbar, update links

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,32 +1,73 @@
 <template>
-  <ul class="bg-red-300">
-    <li><router-link :to="{ name: 'home' }">Home</router-link></li>
-    <li><router-link :to="{ name: 'about' }">About</router-link></li>
-    <li><router-link :to="{ name: 'dashboard' }">Dashboard</router-link></li>
-    <li><router-link :to="{ name: 'login' }">Login</router-link></li>
-    <li><router-link :to="{ name: 'login' }" @click="logoutClicked">Logout</router-link></li>
-  </ul>
+  <div class="navbar bg-base-100">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+          </svg>
+        </label>
+
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <li><router-link :to="{ name: 'about' }">關於我們</router-link></li>
+          <li><router-link :to="{ name: 'products' }">所有產品</router-link></li>
+          <li><router-link :to="{ name: 'blog' }">產品文章</router-link></li>
+          <li><router-link :to="{ name: 'contact' }">聯絡我們</router-link></li>
+          <li><router-link :to="{ name: 'login' }" @click="logoutClicked">Logout</router-link></li>
+        </ul>
+      </div>
+      <router-link :to="{ name: 'home' }" class="btn btn-ghost normal-case text-xl">
+        <svg
+          width="25"
+          height="25"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          class="fill-current"
+        >
+          <path
+            d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"
+          ></path>
+        </svg>
+        <span class="ml-2 mb-1"> Logo </span>
+      </router-link>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li><router-link :to="{ name: 'about' }">關於我們</router-link></li>
+        <li><router-link :to="{ name: 'products' }">所有產品</router-link></li>
+        <li><router-link :to="{ name: 'blog' }">產品文章</router-link></li>
+        <li><router-link :to="{ name: 'contact' }">聯絡我們</router-link></li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <ul class="flex">
+        <li>
+          <router-link class="btn btn-square btn-ghost" :to="{ name: 'cart' }">
+            <svg title="購物車 Cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" width="24">
+              <path
+                d="M7 22q-.825 0-1.412-.587Q5 20.825 5 20q0-.825.588-1.413Q6.175 18 7 18t1.412.587Q9 19.175 9 20q0 .825-.588 1.413Q7.825 22 7 22Zm10 0q-.825 0-1.412-.587Q15 20.825 15 20q0-.825.588-1.413Q16.175 18 17 18t1.413.587Q19 19.175 19 20q0 .825-.587 1.413Q17.825 22 17 22ZM6.15 6l2.4 5h7l2.75-5ZM5.2 4h14.75q.575 0 .875.512.3.513.025 1.038l-3.55 6.4q-.275.5-.738.775Q16.1 13 15.55 13H8.1L7 15h12v2H7q-1.125 0-1.7-.988-.575-.987-.05-1.962L6.6 11.6 3 4H1V2h3.25Zm3.35 7h7Z"
+              />
+            </svg>
+          </router-link>
+        </li>
+        <li>
+          <button href="#" class="btn btn-square btn-ghost">
+            <svg
+              title="喜愛產品 Favorite"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              width="24"
+            >
+              <path
+                d="m12 21-1.45-1.3q-2.525-2.275-4.175-3.925T3.75 12.812Q2.775 11.5 2.388 10.4 2 9.3 2 8.15 2 5.8 3.575 4.225 5.15 2.65 7.5 2.65q1.3 0 2.475.55T12 4.75q.85-1 2.025-1.55 1.175-.55 2.475-.55 2.35 0 3.925 1.575Q22 5.8 22 8.15q0 1.15-.387 2.25-.388 1.1-1.363 2.412-.975 1.313-2.625 2.963-1.65 1.65-4.175 3.925Zm0-2.7q2.4-2.15 3.95-3.688 1.55-1.537 2.45-2.674.9-1.138 1.25-2.026.35-.887.35-1.762 0-1.5-1-2.5t-2.5-1q-1.175 0-2.175.662-1 .663-1.375 1.688h-1.9q-.375-1.025-1.375-1.688-1-.662-2.175-.662-1.5 0-2.5 1t-1 2.5q0 .875.35 1.762.35.888 1.25 2.026.9 1.137 2.45 2.674Q9.6 16.15 12 18.3Zm0-6.825Z"
+              />
+            </svg>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
-
-<script setup>
-import getAuthToken from '@/helper/getAuthToken';
-import { useToast } from 'vue-toastification';
-import axios from 'axios';
-
-function logoutClicked() {
-  axios({
-    method: 'post',
-    url: `${import.meta.env.VITE_APP_API}/logout`,
-    headers: {
-      Authorization: getAuthToken(),
-    },
-  })
-    .then((res) => {
-      if (!res.data.success) throw new Error(`${res.data.message}`);
-      useToast().success(`${res.data.message}`);
-    })
-    .catch((err) => {
-      useToast().error(`${err.message}`);
-    });
-}
-</script>
