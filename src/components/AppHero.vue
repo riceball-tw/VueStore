fc7eae0fd1ab93b4059b7c0cb36f9d5e3a062d5c
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-27 22:59:51 +0800

    feat: Basic hero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hero bg-base-200">
+    <div class="hero-content py-8 text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">{{ title }}</h1>
+        <p class="py-6">
+          {{ description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+    default() {
+      return '預設標題';
+    },
+  },
+  description: {
+    type: String,
+    default() {
+      return '';
+    },
+  },
+});
+</script>
