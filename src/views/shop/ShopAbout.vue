b234fdad6bb9234d8248321756817441814062cf
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-27 23:06:59 +0800

    feat: Basic about page

--- src/views/shop/ShopAbout.vue
+++ src/views/shop/ShopAbout.vue
@@ -1,5 +1,7 @@
-<script setup></script>
+<script setup>
+import Hero from '@/components/AppHero.vue';
+</script>
 
 <template>
-  <h1>About</h1>
+  <Hero title="關於我們" description="一些關於這個網站的描述可撰寫於此。" />
 </template>
