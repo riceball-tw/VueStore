ac797559fb9b39a89971a80583a1a57598be50cc
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-29 01:18:49 +0800

    refactor: apply style

--- src/components/shop/ShopCheckout.vue
+++ src/components/shop/ShopCheckout.vue
@@ -1,56 +1,62 @@
 <template>
   <form>
-    <table v-if="orderData.order">
-      <thead>
-        <th>品名</th>
-        <th>數量</th>
-        <th>單價</th>
-      </thead>
-      <tbody>
-        <tr v-for="product in orderData.order.products" :key="product.id">
-          <td>{{ product.product.title }}</td>
-          <td>{{ product.qty }} / {{ product.product.unit }}</td>
-          <td>{{ product.total }}</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colspan="2">總計</td>
-          <td>100</td>
-        </tr>
-      </tfoot>
-    </table>
+    <div class="overflow-x-auto w-full">
+      <table v-if="orderData.order" class="table table-zebra w-full">
+        <thead>
+          <th>品名</th>
+          <th>數量</th>
+          <th>單價</th>
+        </thead>
+        <tbody>
+          <tr v-for="product in orderData.order.products" :key="product.id">
+            <td>{{ product.product.title }}</td>
+            <td>{{ product.qty }} / {{ product.product.unit }}</td>
+            <td>{{ product.total }}</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="2">總計</td>
+            <td>100</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
     <hr />
-    <table>
-      <tbody v-if="orderData.order">
-        <tr>
-          <th width="100">Email</th>
-          <td>{{ orderData.order.user.email }}</td>
-        </tr>
-        <tr>
-          <th>姓名</th>
-          <td>{{ orderData.order.user.name }}</td>
-        </tr>
-        <tr>
-          <th>收件人電話</th>
-          <td>{{ orderData.order.user.tel }}</td>
-        </tr>
-        <tr>
-          <th>收件人地址</th>
-          <td>{{ orderData.order.user.address }}</td>
-        </tr>
-        <tr>
-          <th>付款狀態</th>
-          <td>
-            <span v-if="!orderData.order.is_paid">尚未付款</span>
-            <span v-else>付款完成</span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="overflow-x-auto w-full">
+      <table class="table table-zebra w-full">
+        <tbody v-if="orderData.order">
+          <tr>
+            <th width="100">Email</th>
+            <td>{{ orderData.order.user.email }}</td>
+          </tr>
+          <tr>
+            <th>姓名</th>
+            <td>{{ orderData.order.user.name }}</td>
+          </tr>
+          <tr>
+            <th>收件人電話</th>
+            <td>{{ orderData.order.user.tel }}</td>
+          </tr>
+          <tr>
+            <th>收件人地址</th>
+            <td>{{ orderData.order.user.address }}</td>
+          </tr>
+          <tr>
+            <th>付款狀態</th>
+            <td>
+              <span v-if="!orderData.order.is_paid">尚未付款</span>
+              <span v-else>付款完成</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </form>
   <div>
-    <button v-if="!orderData?.order?.is_paid" type="button" @click="payOrder(orderId)">確認付款去</button>
+    <button v-if="!orderData?.order?.is_paid" class="btn btn-outline" type="button" @click="payOrder(orderId)">
+      確認付款去
+    </button>
   </div>
 </template>
 
