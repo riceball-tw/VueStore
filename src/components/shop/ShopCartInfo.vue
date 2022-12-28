ffb73943d8d698aeeaeaa89ced3a7c892fbc1861
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-29 01:18:12 +0800

    refactor: new style and seperate checkoutstep component

--- src/components/shop/ShopCartInfo.vue
+++ src/components/shop/ShopCartInfo.vue
@@ -1,8 +1,15 @@
 <template>
-  <div>
-    <ShopCart />
+  <div class="flex justify-center">
+    <CheckOutStep current-step="2" />
+  </div>
+  <div class="flex gap-4 justify-center flex-row-reverse my-8">
+    <div>
+      <h2 class="text-2xl">訂單細節</h2>
+      <p>之後補上</p>
+    </div>
+
     <form
-      class="flex flex-col"
+      class="flex flex-col max-w-lg w-full"
       @submit.prevent="
         (e) => {
           createOrder({
@@ -19,37 +26,85 @@
         }
       "
     >
-      <label>
-        E-mail
-        <input name="email" type="email" placeholder="請輸入您的電子信箱……" required />
-      </label>
+      <h2 class="text-2xl">訂購人資訊</h2>
+      <!-- Email -->
+      <div class="form-control w-full">
+        <label for="email" class="label">
+          <span class="label-text">E-mail</span>
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          class="input input-bordered w-full"
+          placeholder="請輸入您的電子信箱……"
+          required
+        />
+      </div>
 
-      <label>
-        姓名
-        <input name="name" type="text" placeholder="請輸入您的姓名……" required />
-      </label>
-      <label>
-        電話
-        <input name="tel" type="tel" placeholder="請輸入您的地址……" required />
-      </label>
-      <label>
-        地址
-        <input name="address" type="text" placeholder="請輸入您的姓名……" required />
-      </label>
-      <label>
-        附註
-        <input name="message" type="text" placeholder="請輸入您的附註……" />
-      </label>
-      <button type="submit">確認付款去</button>
+      <!-- Name -->
+      <div class="form-control w-full">
+        <label for="name" class="label">
+          <span class="label-text">姓名</span>
+        </label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          class="input input-bordered w-full"
+          placeholder="請輸入您的姓名……"
+          required
+        />
+      </div>
+
+      <!-- Name -->
+      <div class="form-control w-full">
+        <label for="tel" class="label">
+          <span class="label-text">電話</span>
+        </label>
+        <input
+          id="tel"
+          name="tel"
+          type="tel"
+          class="input input-bordered w-full"
+          placeholder="請輸入您的電話……"
+          required
+        />
+      </div>
+
+      <!-- Name -->
+      <div class="form-control w-full">
+        <label for="address" class="label">
+          <span class="label-text">地址</span>
+        </label>
+        <input
+          id="address"
+          name="address"
+          type="text"
+          class="input input-bordered w-full"
+          placeholder="請輸入您的地址……"
+          required
+        />
+      </div>
+
+      <!-- Description -->
+      <div class="form-control w-full">
+        <label for="message" class="label">
+          <span class="label-text">附註</span>
+        </label>
+        <textarea id="message" name="message" class="textarea input-bordered" placeholder="請輸入您的附註……"></textarea>
+      </div>
+
+      <button class="btn btn-outline" type="submit">確認付款去</button>
     </form>
   </div>
 </template>
 
 <script setup>
+import CheckOutStep from '@/components/ShopCheckoutStep.vue';
 import axios from 'axios';
 import getAuthToken from '@/helper/getAuthToken';
 import { useToast } from 'vue-toastification';
-import ShopCart from '@/components/shop/ShopCart.vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
