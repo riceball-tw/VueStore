b5989dc6aef2f14f61733a0894f988262f92552a
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-29 01:13:20 +0800

    feat: seperate checkout step to new component

---
+++ src/components/ShopCheckoutStep.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="steps">
+    <router-link
+      v-for="(step, index) in stepsData"
+      :key="step.title"
+      class="step"
+      :class="{ 'step-primary': index < props.currentStep }"
+      :to="step.link"
+    >
+      {{ step.title }}
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  currentStep: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const stepsData = [
+  {
+    title: '確認購物車品項',
+    link: 'cart',
+  },
+  {
+    title: '填寫訂單資料',
+    link: 'cartInfo',
+  },
+  {
+    title: '確認訂單資料並付款',
+    link: 'checkout',
+  },
+];
+</script>
