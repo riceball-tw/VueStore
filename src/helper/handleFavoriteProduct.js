31975af0ca9a6dc0551aee03205a163e95066ebc
Riceball <42161586+Anderice@users.noreply.github.com>
2022-12-31 11:36:08 +0800

    feat: Add single product page myfavorite functionality

---
+++ src/helper/handleFavoriteProduct.js
@@ -0,0 +1,25 @@
+import { useToast } from 'vue-toastification';
+
+function setFavoriteProduct(id) {
+  localStorage.setItem('favoriteProduct', JSON.stringify(id));
+}
+
+function getFavoriteProducts() {
+  return JSON.parse(localStorage.getItem('favoriteProduct')) ?? [];
+}
+
+export function checkIsProductFavorited(targetId) {
+  return getFavoriteProducts().includes(targetId);
+}
+
+export function toggleFavoriteProduct(targetId, targetName) {
+  let favoriteProducts = getFavoriteProducts();
+  if (favoriteProducts.includes(targetId)) {
+    favoriteProducts = favoriteProducts.filter((item) => item !== targetId);
+    useToast().info(`已將 ${targetName} 從收藏中移除 `);
+  } else {
+    favoriteProducts = [...favoriteProducts, targetId];
+    useToast().info(`已將 ${targetName} 加入收藏 `);
+  }
+  setFavoriteProduct(favoriteProducts);
+}
