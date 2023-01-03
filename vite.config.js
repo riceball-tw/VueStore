8c48ba18d69b920b8152eb18ffb50b39ef1cc190
Riceball <42161586+Anderice@users.noreply.github.com>
2023-01-03 10:19:26 +0800

    chore: github page

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/VueStore/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
