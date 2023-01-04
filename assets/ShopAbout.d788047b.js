3607f2cf143c87a121c346de41e939705eab95e7
riceball-tw <riceball-tw@users.noreply.github.com>
2023-01-04 05:20:10 +0000

    deploy: 189ae755dc95705a89a9d7cae101485c62476f64

--- assets/ShopAbout.d788047b.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./AppHero.1c3d8770.js";import{o as t,h as e}from"./index.ddd01c4b.js";const p={__name:"ShopAbout",setup(r){return(_,a)=>(t(),e(o,{title:"\u95DC\u65BC\u6211\u5011",description:"\u4E00\u4E9B\u95DC\u65BC\u9019\u500B\u7DB2\u7AD9\u7684\u63CF\u8FF0\u53EF\u64B0\u5BEB\u65BC\u6B64\u3002"}))}};export{p as default};
